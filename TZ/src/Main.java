88caca55f7af215c541ce5a12327c6cc80706cde
VMironovV <43193863+VMironovV@users.noreply.github.com>
2022-02-06 22:19:22 +0700

    basis

---
+++ TZ/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args){
+        switch (args[0]){
+            case()
+        }
+        for (int i = 0; i < args.length; i++){
+
+        }
+    }
+}
